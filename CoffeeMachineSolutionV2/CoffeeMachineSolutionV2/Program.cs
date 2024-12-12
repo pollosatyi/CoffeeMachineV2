8065a943356b81f9c166bd5eb50ecffa2253f392
hghgrdghtdrtrthry <88507116+pollosatyi@users.noreply.github.com>
2024-12-12 14:01:29 +0300

    Update Program.cs

--- CoffeeMachineSolutionV2/CoffeeMachineSolutionV2/Program.cs
+++ CoffeeMachineSolutionV2/CoffeeMachineSolutionV2/Program.cs
@@ -1,15 +1,12 @@
 
 ﻿using CoffeeMachineSolutionV2.Coffees;
-
 namespace CoffeeMachineSolutionV2;
-
 
 
 class Program
 {
     static void Main(string[] args)
     {
-
         var orderOfCoffee=CoffeeCreate.CreateCoffee();
         CoffeeOrder.CoffeeOrderPrint(orderOfCoffee);
     }
